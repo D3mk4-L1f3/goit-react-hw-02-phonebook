a3516ead1c638d2e98215937268e9c93060e7129
D3mk4 <dmtvchenko@gmail.com>
2023-09-10 03:16:39 +0300

    addedd 1.1

---
+++ src/components/module/Filter.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Filter({ onFilter, filter }) {
+  return (
+    <div>
+      <h3>Find contacts by name</h3>
+      <input onChange={onFilter} type="text" value={filter} />
+    </div>
+  );
+}
+
+Filter.propTypes = {
+  onFilter: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+};
+
+export default Filter;
